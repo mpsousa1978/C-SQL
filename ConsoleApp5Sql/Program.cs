6850de106bae9c3617926cd32e4c9fe79115d837
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2023-08-30 13:35:45 -0500

    Add files via upload

---
+++ ConsoleApp5Sql/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace ConsoleApp5Sql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.\\SQLEXPRESS;Database=BeautySolun;Integrated Security=SSPI;TrustServerCertificate=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string sqlQuery = "select p.id_professional,p.Name,s.Description dsc_status From bt_professional p inner join bt_status s on p.id_status = s.id_status   ";
+                    SqlCommand cnd = new SqlCommand(sqlQuery, connection);
+
+                    using (SqlDataReader reader = cnd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["id_professional"]} - {reader["Name"]} - status {reader["dsc_status"]}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error:{ex.ToString()}");
+                }
+
+            }
+        }
+    }
+}
